172c34a8fe6304f459653559a468eac2d9d60880
a-fedorov <alex.fedorov1991@gmail.com>
2014-09-15 13:56:52 +0300

    Change fonts size

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -14,13 +14,13 @@ Match3Game.MainMenu.prototype = {
 
     //start game text
     var text = "Click to begin";
-    var style = { font: "30px Arial", fill: "#fff", align: "center" };
+    var style = { font: "50px Arial", fill: "#fff", align: "center" };
     var t = this.game.add.text(this.game.width/2, this.game.height/2 - 100, text, style);
     t.anchor.set(0.5);
 
     //highest score
     text = "Highest score: "+this.highestScore;
-    style = { font: "15px Arial", fill: "#fff", align: "center" };
+    style = { font: "20px Arial", fill: "#fff", align: "center" };
 
     var h = this.game.add.text(this.game.width/2, this.game.height/2 - 25, text, style);
     h.anchor.set(0.5);
