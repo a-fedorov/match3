91f182434d31d17fa2e238832e96c60184ca8fdf
a-fedorov <alex.fedorov1991@gmail.com>
2014-09-19 15:35:48 +0300

    Change background color

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -4,20 +4,14 @@ Match3Game.MainMenu = function(){};
 
 Match3Game.MainMenu.prototype = {
   create: function() {
-    this.game.stage.backgroundColor = '#000';
-    
+    this.game.stage.backgroundColor = '#222';
+
     //start game text
     var text = "Click to begin";
-    var style = { font: "50px Arial", fill: "#fff", align: "center" };
+    var style = { font: "50px Arial", fill: "#eee", align: "center" };
     var t = this.game.add.text(this.game.width/2, this.game.height/2 - 100, text, style);
     t.anchor.set(0.5);
-
-    //highest score
-    text = "Highest score: " + this.highestScore;
-    style = { font: "20px Arial", fill: "#fff", align: "center" };
-
-    var h = this.game.add.text(this.game.width/2, this.game.height/2 - 25, text, style);
-    h.anchor.set(0.5);
+    
   },
   
   update: function() {
