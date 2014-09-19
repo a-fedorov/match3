6ad8024c60ea2e5f6b0d49cccf1788476601649a
a-fedorov <alex.fedorov1991@gmail.com>
2014-09-19 15:37:25 +0300

    Change game size to 1024x768

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var Match3Game = Match3Game || {};
 
-Match3Game.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '');
+Match3Game.game = new Phaser.Game(1024, 768, Phaser.AUTO, 'game');
 
 Match3Game.game.state.add('Boot', Match3Game.Boot);
 Match3Game.game.state.add('Preload', Match3Game.Preload);
